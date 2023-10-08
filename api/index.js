cea421376eec26398603f61a90601d09e4d10dd9
SuayMack <pry_chu@hotmail.com>
2023-10-08 14:56:15 -0300

    :card_index: feat: connect to database

--- api/index.js
+++ api/index.js
@@ -1,12 +1,21 @@
 import express from 'express'
+import mongoose from "mongoose"
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => { console.log('🎉😁 - Connected to MongoDB! - 🙌')})
+  .catch((error) => {console.log('😢😢 - Error connecting to MongoDB:', error)})
+;
 
 app.listen(port, () => {
   console.log(`🎉😁 - Server is running on ${port}! - 🙌`)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
