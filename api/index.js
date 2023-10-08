c5619088174398eb6c1647e400d123959100c99f
SuayMack <pry_chu@hotmail.com>
2023-10-08 16:59:12 -0300

    :rotating_light: test: create a test api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import mongoose from "mongoose"
 import dotenv from 'dotenv'
 
+import userRouter from './routes/user.route.js'
+
 dotenv.config()
 
 const app = express()
@@ -16,6 +18,4 @@ app.listen(port, () => {
   console.log(`🎉😁 - Server is running on ${port}! - 🙌`)
 })
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/api/user', userRouter)
