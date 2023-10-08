2ef538691e0a43eff7e212242ffc155667be18f4
SuayMack <pry_chu@hotmail.com>
2023-10-08 20:50:23 -0300

    :truck: refactor: Structure change, folder creation

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -16,12 +16,6 @@ export default function Header() {
       <Link to={"/signin"}>
         <p>SignIn</p>
       </Link>   
-      {/* <FaHands />
-      <p>Bem vindo(a), Priscila </p>
-      <IoMdCalendar />
-      <p>03/10/2023</p>
-      <Link to={"/profile"}> <img src="https://picsum.photos/id/237/200" alt="" className={"img"} /></Link>
-      <img src="https://picsum.photos/id/237/200" alt="" className={"img"} /> */}      
     </HeaderStyle>
     
   )
