2a1bcc3c60370a2d560e7bfcd470f9555eb33953
SuayMack <pry_chu@hotmail.com>
2023-10-08 19:19:08 -0300

    :sparkles: feat: create middleware and a function to possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js'
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body
   const hashedPassword = bcryptjs.hashSync(password, 10)
   const newUser = new User({ username, email, password: hashedPassword })
@@ -9,6 +10,6 @@ export const signup = async (req, res) => {
     await newUser.save()
     res.status(201).json("🎉 Usuário criado com sucesso! 😁")
   } catch (error) {
-    res.status(406).json('😢😢 - Usuário ou email já cadastrados:')
+    next(errorHandler(406, 'Usuário ou Email já cadastrado!'))
   }   
 }
