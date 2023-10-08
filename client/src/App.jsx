d99f8dadcfa9728d4308b13ebf91190b1fb8aa43
SuayMack <pry_chu@hotmail.com>
2023-10-08 12:43:34 -0300

    :sparkles: feat: create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home.jsx'
+import Profile from './pages/Profile.jsx'
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
+
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
