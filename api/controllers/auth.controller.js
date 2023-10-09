b58b790b1734a8dc1cf280d5efb892f005cde2ac
SuayMack <pry_chu@hotmail.com>
2023-10-09 12:20:59 -0300

    :sparkles: feat: create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
-import User from '../models/user.model.js'
-import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js'
+import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+import User from '../models/user.model.js'
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body
@@ -12,4 +14,26 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(errorHandler(406, 'Usuário ou Email já cadastrado!'))
   }   
+}
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body
+  try {
+    const validUser = await User.findOne({ email })
+    if (!validUser) return next(errorHandler(404, 'Usuário ou Email não encontrado!'))
+
+    const isPasswordCorrect = bcryptjs.compareSync(password, validUser.password)
+    if (!isPasswordCorrect) return next(errorHandler(401, 'Usuário e/ou Senha incorretos!'))
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+    // para não enviar o password
+    const { password: password, ...rest } = validUser._doc
+    res
+      .cookie('token', token, { httpOnly: true })
+      .status(200)
+      .json(rest)
+    //ex como colocar data de expirção do cookie
+    //res.cookie('token', token, { httpOnly: true, expires: new Date(Date.now() + 1000 * 60 * 60 * 24) })
+  } catch (error) {
+    next(errorHandler(404, 'Usuário ou Email não encontrado!'))
+  }
 }
