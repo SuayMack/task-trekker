1e7b3723bff77c64b32dc2a710edd1b43226a74b
SuayMack <pry_chu@hotmail.com>
2023-10-09 13:25:58 -0300

    :bug: fix: Fixing the sign-in page bug

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -16,19 +16,26 @@ export const signup = async (req, res, next) => {
   }   
 }
 
-export const login = async (req, res, next) => {
+export const signin = async (req, res, next) => {
+  
+  //desestruturar email e password
   const { email, password } = req.body
   try {
     const validUser = await User.findOne({ email })
     if (!validUser) return next(errorHandler(404, 'Usuário ou Email não encontrado!'))
 
-    const isPasswordCorrect = bcryptjs.compareSync(password, validUser.password)
-    if (!isPasswordCorrect) return next(errorHandler(401, 'Usuário e/ou Senha incorretos!'))
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Usuário e/ou Senha incorretos!'))
+    //se tudo correr bem, cria o token com o jwt
+    // passar o id do usuário e o secret
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
-    // para não enviar o password
-    const { password: password, ...rest } = validUser._doc
+    // para não enviar o password:
+    const { password: pass, ...rest } = validUser._doc
+    //usamos o rest para excluir a propriedade password
+    //usamos o httpOnly para ocultar o token e deixar mais seguro
+    //usamos o json para retornar o token
     res
-      .cookie('token', token, { httpOnly: true })
+      .cookie('access_token', token, { httpOnly: true })
       .status(200)
       .json(rest)
     //ex como colocar data de expirção do cookie
