1950b6b61bb1b28f9d40701c11c6a3035617d305
SuayMack <pry_chu@hotmail.com>
2023-10-09 22:06:43 -0300

    :sparkles: feat: adjust the style of the sign in and sign up components

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,5 +1,23 @@
+import { useSelector } from "react-redux";
+
+import { SignupStyle } from './../SignUp/signupStyle';
+
 export default function Profile() {
+  const { currentUser } = useSelector(state => state.user)
   return (
-    <div>Profile</div>
+    <SignupStyle>
+      <h1>Perfil</h1>
+      <form className={"signup"}>
+        <img src={currentUser.avatar} alt="perfil" />
+        <input type="text" placeholder="Username" id="username" className={"input"} />
+        <input type="email" placeholder="Email" id="email" className={"input"} />
+        <input type="password" name="password" placeholder="Senha" id="password" className={"input"} />
+        <button type="submit" className={"signupButton"}>Atualizar</button>        
+      </form>
+      <div className="toSignin">
+        <span className={"deleteAccount"}>Deletar conta</span>
+        <span className={"signinLink"}>Sair</span>
+      </div>
+    </SignupStyle>
   )
 }
