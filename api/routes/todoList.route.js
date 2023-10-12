2868a856cba5c5c85eaea76aec90d524de0be383
SuayMack <pry_chu@hotmail.com>
2023-10-12 10:21:12 -0300

    :sparkles: feat: created todo list create

--- api/routes/todoList.route.js
+++ api/routes/todoList.route.js
@@ -4,6 +4,6 @@ import { verifyToken } from '../utils/verifyUser.js'
 
 const router = express.Router()
 
-router.post('/create', verifyToken, createTodoList )
+router.post('/create', verifyToken, createTodoList)
 
 export default router
--- client/src/pages/CreateTodoList/CreateTodoList.jsx
+++ client/src/pages/CreateTodoList/CreateTodoList.jsx
@@ -1,29 +1,85 @@
-import { SignupStyle } from "../style/signupStyle";
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { SignupStyle } from '../style/signupStyle.js'
 
 export default function CreateTodoList() {
+  const navigate = useNavigate()
+  const { currentUser } = useSelector((state) => state.user)
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    status: "a_fazer",
+    userRef: currentUser._id
+  })
+
+  const  [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+  console.log(formData)
+
+  const handleChange = (e) => {
+    if(e.target.id === 'title'){
+      setFormData({...formData, title: e.target.value})
+    }
+    if(e.target.id === 'description'){
+      setFormData({...formData, description: e.target.value})
+    }
+    if(e.target.id === 'a_fazer' || e.target.id === 'fazendo' || e.target.id === 'feito'){
+      setFormData({...formData, status: e.target.id})
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      setLoading(true)
+      setError(false)
+      const res = await fetch('/api/todolist/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ...formData,
+          userRef: currentUser._id,
+        }),
+      })
+      const data = await res.json()
+      setLoading(false)
+      if(data.success === false){
+        setError(data.message)
+      }
+      navigate(`/todolist/${data._id}`)
+    } catch (error) {
+      setError(error.message)
+      setLoading(false)
+    }
+  }
+
   return (
     <SignupStyle>
       <h1>Create Todo List</h1>
       <div className={"form"}>
-        <form className={"signup"}>
-          <input type="text" id="title" placeholder="Título" maxLength='62' minLength='3' className={"input"} required />
-          <textarea id="description" placeholder="Descrição" className={"input"} cols="30" rows="6"></textarea>
+        <form onSubmit={handleSubmit} className={"signup"}>
+          <input onChange={handleChange} value={formData.title} type="text" id="title" placeholder="Título" maxLength='62' minLength='3' className={"input"} required />
+          <textarea onChange={handleChange} value={formData.description} id="description" placeholder="Descrição" className={"input"} cols="30" rows="6"></textarea>
         
           <div className={"todoList"}>
             <div className={"todoStatus"}>
-              <input type="radio" name="status" id="a fazer" value="a fazer" />
-              <label htmlFor="a fazer">A fazer</label>
+              <input type="radio" id="a_fazer" onChange={handleChange} checked={formData.status === "a_fazer"} />
+              <label htmlFor="a_fazer">A fazer</label>
             </div>
             <div className={"todoStatus"}>
-              <input type="radio" name="status" id="fazendo" value="fazendo" />
+              <input type="radio" id="fazendo" onChange={handleChange} checked={formData.status === "fazendo"} />
               <label htmlFor="fazendo">Fazendo</label>
             </div>
             <div className={"todoStatus"}>
-              <input type="radio" name="status" id="feito" value="feito" />
+              <input type="radio" id="feito" onChange={handleChange} checked={formData.status === "feito"} />
               <label htmlFor="feito">Feito</label>
             </div>
           </div>        
-          <button type="submit" className={"buttonCreateTodoList"}>Criar tarefa</button>   
+          <button type="submit" className={"buttonCreateTodoList"}>{loading ? "Carregando..." : "Criar tarefa"}</button>   
+          {error && <p className={"error"}>{error}</p>}
         </form>
       </div>
     </SignupStyle>
