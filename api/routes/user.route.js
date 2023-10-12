8580ccbcdfcdc71e3f26cfdaa12af9a415423e26
SuayMack <pry_chu@hotmail.com>
2023-10-12 18:27:31 -0300

    :art: structure: route adjustment

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -8,6 +8,6 @@ const router = express.Router()
 router.get('/test',test)
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
-router.get('/todoslist/:id', verifyToken, getUserTodoslist)
+router.get('/todolist/:id', verifyToken, getUserTodoslist)
 
 export default router
